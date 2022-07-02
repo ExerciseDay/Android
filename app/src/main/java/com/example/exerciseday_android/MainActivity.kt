77caac7012a4e90018205437336ef528af80a4ee
mouse0429 <mouse0429@naver.com>
2022-07-02 18:00:40 +0900

    바텀네비게이션 추가
    바텀네비게이션 추가

--- app/src/main/java/com/example/exerciseday_android/MainActivity.kt
+++ app/src/main/java/com/example/exerciseday_android/MainActivity.kt
@@ -2,10 +2,57 @@ package com.example.exerciseday_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.exerciseday_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun initBottomNavigation(){
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, HomeFragment())
+            .commitAllowingStateLoss()
+
+        binding.bottomNavV.setOnItemSelectedListener{ item ->
+            when (item.itemId) {
+                R.id.recordFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, RecordFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.communityFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, CommunityFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.homeFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, HomeFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.mapFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, MapFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.settingFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, SettingFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
