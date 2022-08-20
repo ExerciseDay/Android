c4c6f9bf2a6a5e55ba2a4a90717b6d5b5f2214d8
mouse0429 <mouse0429@naver.com>
2022-08-20 22:05:23 +0900

    바텀네비게이션 시작탭 버그 수정
    바텀네비게이션 시작탭 버그 수정

--- app/src/main/java/com/example/exerciseday_android/MainActivity.kt
+++ app/src/main/java/com/example/exerciseday_android/MainActivity.kt
@@ -18,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.bottomNavV.selectedItemId = R.id.homeFragment
+
         initBottomNavigation()
     }
 
